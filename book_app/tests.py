22fb7ea50f2fd846552252dfd6a7e78e485dec98
rahuugitit <rk8449022182@gmail.com>
2024-09-15 22:13:07 +0530

    this is the add new user

--- book_app/tests.py
+++ book_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Book
+from datetime import date
 # Create your tests here.
+class Firsttestcase(TestCase):
+    def setUp(self):
+        # Setting up test data before running the test
+        Book.objects.create(title="Test Book", author="John Doe", published_date=date(2022, 1, 1))
+
+    def test_equal(self):
+        self.assertEqual(1,1)
